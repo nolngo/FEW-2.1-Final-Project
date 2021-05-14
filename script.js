c1dc2222ffd20ac61d47e72b16fde8caec7e50d5
Nolan Ngo <nolanngo@protonmail.com>
2021-05-14 17:32:43 -0400

    fully functional

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-function getTeam(teamName) {
-  const data = null;
-  const xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
-  xhr.addEventListener('readystatechange', function () {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
-    }
-  });
-
-  xhr.open('GET', `https://api-nba-v1.p.rapidapi.com/teams/nickName/${teamName}`);
-  xhr.setRequestHeader('x-rapidapi-key', 'cc18e95e0fmsh1e8f5597163251cp1dc923jsnf4b5c9f80b9e');
-  xhr.setRequestHeader('x-rapidapi-host', 'api-nba-v1.p.rapidapi.com');
-
-  xhr.send(data);
+function getRecipe(url, successCallback) {
+  fetch("https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi", {
+    "method": "POST",
+    "headers": {
+      "content-type": "text/plain",
+      "x-rapidapi-key": "cc18e95e0fmsh1e8f5597163251cp1dc923jsnf4b5c9f80b9e",
+      "x-rapidapi-host": "mycookbook-io1.p.rapidapi.com"
+    },
+    "body": url
+  })
+  .then(response => response.json()) // Getting the actual response data
+  .then(data => {         
+      successCallback(data)
+      console.log(data)
+  })
+  .catch(err => {
+      console.log(err);
+  }); 
 }
